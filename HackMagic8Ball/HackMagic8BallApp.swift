f1768a88a7fbc5a0cc783fbfaf14abce414a0dc6
Jennifer Allan <jenallan@students.hackleyschool.org>
2023-01-30 11:41:32 -0500

    Initial Commit

---
+++ HackMagic8Ball/HackMagic8BallApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackMagic8BallApp.swift
+//  HackMagic8Ball
+//
+//  Created by Jennifer Allan on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct HackMagic8BallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
