6bc99cd0193b51308b3961e8793ee47cd496620b
Jennifer Allan <jenallan@students.hackleyschool.org>
2023-02-03 09:05:58 -0500

    image?

--- HackMagic8Ball/ContentView.swift
+++ HackMagic8Ball/ContentView.swift
@@ -9,11 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+      
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hackley Magic 8 Ball")
+                .font(.custom(
+                "AmericanTypewriter",
+                fixedSize:36))
+    
+            Button {
+                print("Button pressed")
+            } label: {
+                Image("8ball")
+            .frame(width: 100,height:100)
+          
+            }
+            .contentShape(Rectangle())
+            
+            Text("Click the 8 Ball to Start")
+                .font(.custom(
+                "AmericanTypewriter",
+                fixedSize:36))
         }
         .padding()
     }
