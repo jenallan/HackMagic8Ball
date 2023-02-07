5e20c69ac1d1377338807b3609440b5cf4b948d5
Jennifer Allan <jenallan@students.hackleyschool.org>
2023-02-07 10:02:58 -0500

    current 2/7

--- HackMagic8Ball/LoadingScreen.swift
+++ HackMagic8Ball/LoadingScreen.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  LoadingScreen.swift
 //  HackMagic8Ball
 //
 //  Created by Jennifer Allan on 1/30/23.
@@ -19,15 +19,19 @@ struct ContentView: View {
             Button {
                 print("Button pressed")
             } label: {
-                Image("8ball")
-            .frame(width: 100,height:100)
-          
+                Image("8Ball")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 250, height: 250)
+//                Image(systemName: "8.circle.fill")
+//                    .foregroundColor(.black)
+//                    .font(.system(size: 250))
             }
             .contentShape(Rectangle())
             
             Text("Click the 8 Ball to Start")
                 .font(.custom(
-                "AmericanTypewriter",
+                "Arial",
                 fixedSize:36))
         }
         .padding()
---
+++ HackMagic8Ball/QuestionSelection.swift
@@ -0,0 +1,8 @@
+//
+//  QuestionSelection.swift
+//  HackMagic8Ball
+//
+//  Created by Jennifer Allan on 2/3/23.
+//
+
+import Foundation
