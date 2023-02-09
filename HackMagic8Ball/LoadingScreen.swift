8a3921195364985d4e52c1fab676247e4e57cd0e
Jennifer Allan <jenallan@students.hackleyschool.org>
2023-02-09 11:47:30 -0500

    3 pages in app, code separated into different files

--- HackMagic8Ball/LoadingScreen.swift
+++ HackMagic8Ball/LoadingScreen.swift
@@ -8,57 +8,6 @@
 import SwiftUI
 
 struct LoadingView: View {
-    struct ContentView: View{
-        @State private var startingValue: String = ""
-        let units = ["English Teacher","History Teacher","Math Teacher","Science Teacher", "Language Teacher", "Art Teacher", "Comp Sci Teacher", "Dean", "Administrator", "Hackley Fan Faves"]
-        let extraUnits = ["None", "English Teacher","History Teacher","Math Teacher","Science Teacher", "Language Teacher", "Art Teacher", "Comp Sci Teacher", "Dean", "Administrator", "Hackley Fan Faves"]
-        @State private var startingUnit = "Hackley Fan Faves"
-        @State private var extraStartingUnit = "None"
-        var body: some View {
-            Form{
-                Section{
-                    TextField("Enter your question", text: $startingValue)
-                }
-            header:
-                {
-                    Text("Question")
-                        .font(.system(size: 15))
-                }
-                Section{
-                    Picker("Type #1", selection: $startingUnit){
-                        ForEach(units, id: \.self){
-                            Text("\($0)")
-                        }
-                    }
-                }header:{
-                    Text("Response Type")
-                        .font(.system(size: 15))
-                }
-                Section{
-                    Picker("Type #2", selection: $extraStartingUnit){
-                        ForEach(extraUnits, id: \.self){
-                            Text("\($0)")
-                        }
-                    }
-                }
-                Section{
-                    Picker("Type #3", selection: $extraStartingUnit){
-                        ForEach(extraUnits, id: \.self){
-                            Text("\($0)")
-                        }
-                    }
-                }
-            }
-// THIS SHOULD MAKE THE BACKGROUND OF THE FORM PURPLE BUT NOT THE ACTUAL INPUT SPOTS BUT IT ISN'T WORKING IDK WHY
-//            .background(Color.purple)
-//            .onAppear {
-//              UITableView.appearance().backgroundColor = .clear
-//            }
-//            .onDisappear {
-//              UITableView.appearance().backgroundColor = .systemGroupedBackground
-//            }
-        }
-    }
     
     var body: some View {
         
@@ -73,7 +22,7 @@ struct LoadingView: View {
                 Text("click the 8 ball to start").font(.custom("Papyrus", size: 30))
                     .foregroundColor(.white).offset(x: 0, y:220)
                 
-                NavigationLink(destination: ContentView())
+                NavigationLink(destination: QuestionSelectionView())
                 {  Image("8Ball")
                         .resizable()
                         .scaledToFit()
---
+++ HackMagic8Ball/ShakeView.swift
@@ -0,0 +1,20 @@
+//
+//  ShakeView.swift
+//  HackMagic8Ball
+//
+//  Created by Jennifer Allan on 2/9/23.
+//
+
+import SwiftUI
+
+struct ShakeView: View{
+    var body: some View {
+        Text("TEST")
+    }
+}
+
+struct ShakeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShakeView()
+    }
+}
